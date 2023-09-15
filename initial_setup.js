681d21642cfdc8f9bbba6eb3c49e0b2ef88cbf70
nahid6970 <nahid6970@gmail.com>
2023-09-15 15:36:11 +0600

    XX

--- initial_setup.js
+++ initial_setup.js
@@ -1,7 +1,9 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const childContentElement = document.getElementById('childContent');
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'child.html', true);
+    xhr.open('GET', 'initial_setup.html', true); // Update the filename here
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             childContentElement.innerHTML = xhr.responseText;
