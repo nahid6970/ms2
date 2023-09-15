9f8cb07b4f1142e24ec1fe74628578caaf42d90c
nahid6970 <nahid6970@gmail.com>
2023-09-15 15:04:57 +0600

    XX

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get all the spans
+const spans = document.querySelectorAll('span');
+
+// Find the span with the maximum width
+let maxWidth = 0;
+spans.forEach(span => {
+  const width = span.offsetWidth;
+  if (width > maxWidth) {
+    maxWidth = width;
+  }
+});
+
+// Set the width of all spans to the maximum width
+spans.forEach(span => {
+  span.style.width = `${maxWidth}px`;
+});
