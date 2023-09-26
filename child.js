4a53a60976086ce160777362c8928344a342b7c6
nahid6970 <nahid6970@gmail.com>
2023-09-26 12:37:34 +0600

    XX

--- child.js
+++ child.js
@@ -122,3 +122,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+
+
+
--- script.js
+++ script.js
@@ -16,3 +16,63 @@ spans.forEach(span => {
 });
 
 
+
+
+
+
+
+
+
+
+
+function parseMarkdownTable(markdown) {
+  const rows = markdown.trim().split('\n');
+  const headers = rows[1].split('|').map(header => header.trim()).filter(Boolean);
+  const data = rows.slice(2, -1).map(row => row.split('|').map(cell => cell.trim()).filter(Boolean));
+
+  return { headers, data };
+}
+
+function renderTable(headers, data) {
+  const tableElement = document.createElement('table');
+  const theadElement = document.createElement('thead');
+  const tbodyElement = document.createElement('tbody');
+
+  const headerRow = document.createElement('tr');
+  headers.forEach(header => {
+      const thElement = document.createElement('th');
+      thElement.textContent = header;
+      headerRow.appendChild(thElement);
+  });
+
+  theadElement.appendChild(headerRow);
+
+  data.forEach(rowData => {
+      const rowElement = document.createElement('tr');
+      rowData.forEach(cellData => {
+          const tdElement = document.createElement('td');
+          tdElement.textContent = cellData;
+          rowElement.appendChild(tdElement);
+      });
+      tbodyElement.appendChild(rowElement);
+  });
+
+  tableElement.appendChild(theadElement);
+  tableElement.appendChild(tbodyElement);
+
+  return tableElement;
+}
+
+function convertMarkdownTables() {
+  const elements = document.querySelectorAll('[id^="markdown-table-"]');
+  elements.forEach(element => {
+      const markdownContent = element.innerHTML;
+      const { headers, data } = parseMarkdownTable(markdownContent);
+      const tableElement = renderTable(headers, data);
+      element.innerHTML = ''; // Clear the original content
+      element.appendChild(tableElement);
+  });
+}
+
+// Call the function to convert Markdown tables on page load
+window.addEventListener('load', convertMarkdownTables);
