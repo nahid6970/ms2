e6e17d98302b238d79bdbd675df7c269272132be
nahid6970 <nahid6970@gmail.com>
2023-09-26 15:24:57 +0600

    XX

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-/* // Get all the spans
-const spans = document.querySelectorAll('span');
-
-// Find the span with the maximum width
-let maxWidth = 0;
-spans.forEach(span => {
-  const width = span.offsetWidth;
-  if (width > maxWidth) {
-    maxWidth = width;
-  }
-});
-
-// Set the width of all spans to the maximum width
-spans.forEach(span => {
-  span.style.width = `${maxWidth}px`;
-}); */
-
-
-
-
-
-
-
-
-
-
 /* For Showing Markdown Table in HTML properly but lines also show */
-/* function parseMarkdownTable(markdown) {
+function parseMarkdownTable(markdown) {
   const rows = markdown.trim().split('\n');
   const headers = rows[1].split('|').map(header => header.trim()).filter(Boolean);
   const data = rows.slice(2, -1).map(row => row.split('|').map(cell => cell.trim()).filter(Boolean));
@@ -76,7 +50,52 @@ function convertMarkdownTables() {
 
 // Call the function to convert Markdown tables on page load
 window.addEventListener('load', convertMarkdownTables);
- */
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* // Get all the spans
+const spans = document.querySelectorAll('span');
+
+// Find the span with the maximum width
+let maxWidth = 0;
+spans.forEach(span => {
+  const width = span.offsetWidth;
+  if (width > maxWidth) {
+    maxWidth = width;
+  }
+});
+
+// Set the width of all spans to the maximum width
+spans.forEach(span => {
+  span.style.width = `${maxWidth}px`;
+}); */
+
+
+
+
+
+
+
+
+
 
 
 
