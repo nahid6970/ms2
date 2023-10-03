08ab080277431c4af0ba86d625f8a4fb88be7c55
nahid6970 <nahid6970@gmail.com>
2023-10-03 20:37:11 +0600

    XX

--- script/clock.js
+++ script/clock.js
@@ -1,20 +1,27 @@
+Number.prototype.pad = function(n) {
+  for (var r = this.toString(); r.length < n; r = 0 + r);
+  return r;
+};
+
 function updateClock() {
   var now = new Date();
-  var options = { hour: 'numeric', minute: '2-digit', second: '2-digit', hour12: true };
-  var timeString = now.toLocaleTimeString('en-US', options).split(':');
-  
-  document.getElementById("h").firstChild.nodeValue = timeString[0];
-  document.getElementById("m").firstChild.nodeValue = timeString[1];
-  document.getElementById("s").firstChild.nodeValue = timeString[2].split(' ')[0];
-  document.getElementById("ampm").firstChild.nodeValue = timeString[2].split(' ')[1];
-  
-  var mo = now.getMonth(),
+  var sec = now.getSeconds(),
+    min = now.getMinutes(),
+    hou = now.getHours(),
+    mo = now.getMonth(),
     dy = now.getDate(),
     yr = now.getFullYear();
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var tags = ["mon", "d", "y"],
-    corr = [months[mo], dy, yr];
-  
+  var tags = ["mon", "d", "y", "h", "m", "s"], // Removed "mi" from tags
+    corr = [months[mo], dy, yr, (hou % 12 || 12).pad(2), min.pad(2), sec.pad(2)]; // Adjusted hours to 12-hour format, removed milliseconds
   for (var i = 0; i < tags.length; i++)
     document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+  
+  // Add AM/PM
+  document.getElementById("ampm").firstChild.nodeValue = hou >= 12 ? "PM" : "AM";
+}
+
+function initClock() {
+  updateClock();
+  window.setInterval("updateClock()", 1);
 }
