165252a9fded471f05e5d3473d037ca5542b072c
Nahid Ahmed <nahid6970@gmail.com>
2024-01-01 13:38:28 +0600

    XX

--- script/list.js
+++ script/list.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Get the content of the <pre> element
-  var contentElement = document.getElementById('content');
-  var content = contentElement.innerHTML;
+  // Get all elements with the <list> tag
+  var listElements = document.querySelectorAll('list');
 
-  // Process lines and convert those starting with a dash to list items
-  var updatedContent = content.replace(/(^|\n)- (.*?)(?=\n|$)/g, function (match, p1, p2) {
-    return p1 + '<li>' + p2.trim() + '</li>';
+  // Process each <list> element
+  listElements.forEach(function (listElement) {
+    // Get the content of the <list> element
+    var content = listElement.innerHTML;
+
+    // Process lines and convert those starting with a dash to list items
+    var updatedContent = content.replace(/(^|\n)- (.*?)(?=\n|$)/g, function (match, p1, p2) {
+      return p1 + '<li class="custom-list">' + p2.trim() + '</li>';
+    });
+
+    // Wrap the modified content with <ul> element
+    listElement.innerHTML = '<ul>' + updatedContent + '</ul>';
   });
-
-  // Update the content of the <pre> element with the modified content
-  contentElement.innerHTML = updatedContent;
 });
