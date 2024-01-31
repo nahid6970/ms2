515cc4ef042e3937f5f613d4329513258152f362
Nahid Ahmed <nahid6970@gmail.com>
2024-01-31 21:39:14 +0600

    XX

---
+++ script/open_newtab.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+var links = document.querySelectorAll('a');
+links.forEach(function (link) {
+link.setAttribute('target', '_blank');
+});
+});
+
